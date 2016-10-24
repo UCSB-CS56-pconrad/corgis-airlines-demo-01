ea2f97e9b1b9e9f3f49ef09f0d624509050d3cf9
Phill Conrad <pconrad@cs.ucsb.edu>
2016-10-24 10:02:05 -0700

    initial code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# corgis-airlines-demo-01
+# corgis-tryout-01
+Trying Airlines database from https://think.cs.vt.edu/corgis/java/airlines/airlines.html
---
+++ src/edu/ucsb/cs56/corgis/airlines/demos/ExampleClass.java
@@ -0,0 +1,14 @@
+package edu.ucsb.cs56.corgis.airlines.demos;
+
+import java.util.ArrayList;
+import corgis.airlines.AirlinesLibrary;
+import corgis.airlines.domain.Airline;
+
+public class ExampleClass {
+    public static void main(String[] args) {
+        // Get access to the library
+        AirlinesLibrary airlinesLibrary = new AirlinesLibrary();
+        // Access data inside the library
+        ArrayList<Airline> list_of_airline = airlinesLibrary.getReports(false);
+    }
+}
